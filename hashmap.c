af18c1f68e67b387648e424a41e40d9dde3be76b
Paul Chote <paul@chote.net>
2012-12-14 00:59:08 +1300

    Tweak hashmap to use const keys.

--- hashmap.c
+++ hashmap.c
@@ -15,7 +15,7 @@
 
 /* We need to keep keys and values */
 typedef struct _hashmap_element{
-	char* key;
+	const char* key;
 	int in_use;
 	any_t data;
 } hashmap_element;
@@ -167,7 +167,7 @@ static unsigned long crc32(const unsigned char *s, unsigned int len)
 /*
  * Hashing function for a string
  */
-static unsigned int hashmap_hash_int(hashmap_map * m, char* keystring){
+static unsigned int hashmap_hash_int(hashmap_map * m, const char* keystring){
 
     unsigned long key = crc32((unsigned char*)(keystring), strlen(keystring));
 
@@ -191,7 +191,7 @@ static unsigned int hashmap_hash_int(hashmap_map * m, char* keystring){
  * Return the integer of the location in data
  * to store the point to the item, or MAP_FULL.
  */
-static int hashmap_hash(map_t in, char* key){
+static int hashmap_hash(map_t in, const char* key){
 	int curr;
 	int i;
 
@@ -261,7 +261,7 @@ int hashmap_rehash(map_t in){
 /*
  * Add a pointer to the hashmap with some key
  */
-int hashmap_put(map_t in, char* key, any_t value){
+int hashmap_put(map_t in, const char* key, any_t value){
 	int index;
 	hashmap_map* m;
 
@@ -289,7 +289,7 @@ int hashmap_put(map_t in, char* key, any_t value){
 /*
  * Get your pointer out of the hashmap with a key
  */
-int hashmap_get(map_t in, char* key, any_t *arg){
+int hashmap_get(map_t in, const char* key, any_t *arg){
 	int curr;
 	int i;
 	hashmap_map* m;
@@ -351,7 +351,7 @@ int hashmap_iterate(map_t in, PFany f, any_t item) {
 /*
  * Remove an element with that key from the map
  */
-int hashmap_remove(map_t in, char* key){
+int hashmap_remove(map_t in, const char* key){
 	int i;
 	int curr;
 	hashmap_map* m;
